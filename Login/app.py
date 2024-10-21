bca0f518c85304d6103722a56bdbe01913b7ce17
Abacate <135732758+kimberly034@users.noreply.github.com>
2024-10-21 15:33:51 -0300

    Delete Login directory

--- Login/app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from controllers.controller import blueprint_default as page
-app=Flask(__name__)
-app.register_blueprint(page)
-
-
-if __name__=='__main__':
-    app.run(debug=True)
